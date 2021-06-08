92aba52536b8d8b46ce93023cc76c21cbe6cdcfe
Ilya Petrov <dnamix1@gmail.com>
2021-06-08 11:03:39 +0300

    user's auth state monitor hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,16 @@
-// import React from "react";
-// import * as db from "../firestore";
+import React from "react";
+import * as db from "../firestore";
 
-function useAuth() {}
+function useAuth() {
+  const [user, setUser] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
+    return db.checkAuthState((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+  }, []);
+  return { user, loading };
+}
 
 export default useAuth;
